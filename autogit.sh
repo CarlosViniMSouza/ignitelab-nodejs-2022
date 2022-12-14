b6b9ada559a44286c4fa4942d17c7e9ef4a69664
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-14 11:59:32 -0300

    Lesson 02 -> done

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "Lesson 01 -> done"
+git commit -m "Lesson 02 -> done"
 git push -u origin main
 git status
--- src/infra/http/dtos/create-notification-body.ts
+++ src/infra/http/dtos/create-notification-body.ts
@@ -1,12 +1,12 @@
-import { IsNotEmpty, IsUUID, Length } from "class-validator";
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
-export class CreateNotifBody {
+export class CreateNotificationBody {
     @IsNotEmpty()
     @IsUUID()
     recipientId: string;
 
     @IsNotEmpty()
-    @Length(5, 200)
+    @Length(5, 240)
     content: string;
 
     @IsNotEmpty()
